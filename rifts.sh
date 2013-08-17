347eb6d92a8f303c7e9713448aacba810857e465
h43z <h43zet@gmail.com>
2013-08-17 19:37:22 +0200

    bones

--- rifts.sh
+++ rifts.sh
@@ -1,3 +1,33 @@
+usage(){
+	echo "Usage: NYI"
+}
 
+googleindex(){
+	cat $1 | xmlstarlet sel -T -t -m opml/body/outline -v @xmlUrl -o "###" -o "offline" -o "###" -o "offline" -n >> $db
+}
 
-awk -F'###' '{print $1}' feeds.db | xargs -P 20 -n 1 ./core.sh
+db=feeds.db
+while getopts "hic" OPTION
+do
+	case $OPTION in
+		h)
+			usage
+			exit 0
+		;;
+		g)
+			googleindex $1
+		;;
+		c)
+			if [ -s feeds.db ]; then
+				awk -F'###' '{print $1}' feeds.db | xargs -P 20 -n 1 ./core.sh
+			else
+				echo "feeds.db is empty or does not exist"
+			fi
+			exit 0
+		;;
+		?)
+			usage
+			exit 1
+		;;
+	esac
+done
