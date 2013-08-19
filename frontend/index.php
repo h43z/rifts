98f0fc96ebf65810112fb26acffb7eec6377cc38
h43z <h43zet@gmail.com>
2013-08-19 23:40:52 +0200

    devel

--- frontend/index.php
+++ frontend/index.php
@@ -1,23 +1,56 @@
 <?php
-if(isset($_COOKIE["username"])){
-	echo "Hello ".$_COOKIE["username"];
-}
-else{
-	if(substr($_REQUEST["username"],0,-1) == "2445" && isset($_REQUEST["password"])){
-		// registration valid
-		echo substr($_REQUEST["username"],4,0);	
-		#setcookie (substr($_REQUEST["username"], "", time() - 3600*24*3);
-		echo "nice";
-	}
 
-	echo "LOGIN";
+//GLOBAL VARS
+$registercode="2445";
+$registercodelength=strlen($registercode);
+
+
+function validation($cookie,$user,$pass){
+	if($cookie != null){
+		$flag = $cookie;
+	}else{
+		$flag = $user."_".$pass;
+	}
+	if(file_exists("./userdata/".$flag."_subscription")){
+		setcookie("rifts",$flag,time()+(3600*24*4));
+	}else{
+		echo "login failed!";
+		die();
+	}
+}	
+
+if(isset($_COOKIE["rifts"])){
+	
+	validation($_COOKIE["rifts"]);
+	
+}elseif(isset($_REQUEST["username"]) && isset($_REQUEST["password"])){
+	
+	$code=substr($_REQUEST["username"],0,$registercodelength);
+	$password=md5($_REQUEST["password"]);
+	
+	//tries to register
+	if($code == $registercode){
+		$username=substr($_REQUEST["username"],4,100);
+		file_put_contents("./userdata/".$username."_".$password."_subscription", "");
+		file_put_contents("./userdata/".$username."_".$password."_unread", "");
+		validation(null,$username,$password);
+	}else{
+	//tries to login
+		$username=$_REQUEST["username"];
+		validation(null,$username,$password);
+	}
+}else{
 	echo "<form method=post>";
 	echo "Username: <input type=text name=username>";
 	echo "Password: <input type=password name=password>";
 	echo "<input type=submit>";
 	echo "</form><br>";
+	die();
 }
 
+
+echo "Hello ".$_COOKIE["rifts"]."<br>";
+echo "hier artikel auflisten!";
 
 
 
