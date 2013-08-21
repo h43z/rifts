19cca75214c6d64b348e9c2cbe9cf523be1091e1
h43z <h43zet@gmail.com>
2013-08-22 01:39:40 +0200

    devel

--- frontend/index.php
+++ frontend/index.php
@@ -74,7 +74,7 @@ function getunread(){
 	$data = file($userdata.$id."_unread");
 	foreach ($data as $row) {
 		$counter++;
-		$item = explode("###", $row);
+		$item = explode("###", trim($row));
 		
 		if(empty($item[0]) || empty($item[1])){
 				continue;
@@ -109,8 +109,15 @@ function prepend($path,$str) {
 
 function markasread($url){ // $url == "*all*" == all marked as read
 	global $unreadfile, $historyfile;
-	removeline($unreadfile,$url);
-	if($url !== "*all*"){
+	
+	if($url == "*all*"){
+		$file = file($unreadfile);
+		foreach($file as $line) {
+			prepend($historyfile,$line);
+		}
+		file_put_contents($unreadfile, "");
+	}else{
+		removeline($unreadfile,$url);
 		prepend($historyfile,$url);
 	}
 }
@@ -124,33 +131,53 @@ function addsubscription($url){
 			file_put_contents($subscriptionsfile,$url."\n", FILE_APPEND);
 	}
 }
-// go here after login/registration or valid cookie existence
 
 function removeline($path,$str){
-	global $historyfile;
 	$file = file($path);
 	foreach($file as $key => $line) {
-		if(strpos($line,$str) !== false || $str == "*all*"){
-				unset($file[$key]);
-				if($str == "*all*"){
-					prepend($historyfile,$str);
-				}
+		if(strpos($line,$str) !== false){
+			unset($file[$key]);
 		}
 	}
 	file_put_contents($path,$file);
 }
 
-
-function managefeeds(){
-	
+function getfile($path){
+	$data = parsefile($path);
+	foreach($data as $set){
+		echo "$set[0]<br>";
+	}
 }
 
+
+// go here after login/registration or valid cookie existence
 $id = $_COOKIE["rifts"];
 $unreadfile = $userdata.$id."_unread";
 $historyfile = $userdata.$id."_history";
 $subscriptionsfile = $userdata.$id."_subscriptions";
 
-
+if(isset($_REQUEST["f"])){
+	switch($_REQUEST["f"]){
+		case "add":
+			addsubscription($_REQUEST["url"]);
+		break;
+		case "getsubscriptions":
+			getfile($subscriptionsfile);
+		break;
+		case "allread":
+			markasread("*all*");
+		break;
+		case "markasread":
+			markasread($_REQUEST["url"]);
+		break;
+		case "gethistory":
+			getfile($historyfile);
+		break;
+		default:
+		break;
+	}
+	exit;
+}
 ?>
 <style type="text/css">
         body {font-family: "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Verdana, Tahoma, sans-serif;line-height: 24px;color: #333;font-size:14px}
@@ -169,8 +196,132 @@ $subscriptionsfile = $userdata.$id."_subscriptions";
 
 <html><meta charset='utf-8'/>
 	<title>RIFTS</title>
-	<div id='settings'><b><a href=''>RIFTS (<span id='unseencounter'><? echo count(file($userdata.$id."_unread"));?></span>)</a></b><br><a class=set>add</a><br><a class=set>manage</a><br><a class=set>all read</a><br><a class=set>history</a><br><a class=set>backend</a></div>
+	<div id='settings'>
+	<b><a href=''>RIFTS (<span id='unseencounter'><? echo count(file($userdata.$id."_unread"));?></span>)</a></b><br>
+	<a class=set opt=1>add</a><br>
+	<a class=set opt=2>my feeds</a><br>
+	<a class=set opt=3>all read</a><br>
+	<a class=set opt=4>history</a><br></div>
 	<div id=main><?getunread();?></div>
 </html>
 
+<script>
+linkclick=false;
+window.onload = function() {
+        var links = document.getElementsByClassName("link");
+        for(var i = 0; i < links.length; i++) {
+                links[i].onclick = function(e) {
+                        //e.preventDefault();
+                        //window.open(this.href);  
+                        console.log("clicked on link"); 
+                        url=encodeURI(this.href);
+                        activeitem=this;
+                        linkclick=true;
+                }
+        }
+
+        var links = document.getElementsByClassName("set");
+        for(var i = 0; i < links.length; i++) {
+                links[i].onclick = function() {
+                        links_ = document.getElementsByClassName("set")
+                        for(var x= 0; x <links_.length; x++){
+                                links_[x].style.color = "";
+                        }
+                        this.style.color = "blue";
+                        switch(this.getAttribute("opt")){
+                                case "1":
+                                                var url = window.prompt("RSS URL:","");
+                                                if (url != null && url != ""){
+                                                        ajax("?f=add&url="+encodeURI(url));
+                                                }
+                                        break;
+                                case "2":
+                                                document.getElementById("main").innerHTML = ajax("?f=getsubscriptions");
+                                        break;
+                                case "3":
+                                                var x=window.confirm("Are you sure?")
+                                                if (x){
+                                                        ajax("?f=allread");
+                                                        window.location.reload(false); 
+                                                }
+                                        break;
+                                case "4":
+                                                document.getElementById("main").innerHTML = ajax("?f=gethistory");
+                                        break;
+                        }
+                }
+        }
+
+        var links = document.getElementsByClassName("reddit");
+        for(var i = 0; i < links.length; i++) {
+                links[i].onclick = function() {this.parentNode.remove()}
+        }
+
+        var links = document.getElementsByClassName("close");
+        for(var i = 0; i < links.length; i++) {
+                links[i].onclick = function() {
+					console.log(this.parentNode.children[0].href);
+                        url=encodeURI(this.parentNode.children[0].href);
+                        this.parentNode.remove();
+                        updatecounter();
+                        ajax("?f=markasread&url="+url);
+                };
+        }
+
+}
+
+window.addEventListener('blur', function() {
+        if(linkclick){
+                linkclick=false;
+                if (discuss(url,activeitem)){
+                                return;
+                }
+                activeitem.parentNode.remove();
+                updatecounter();
+                ajax("?f=markasread&url="+url);
+        }
+
+});
+
+function ajax(url){
+        var xhReq = new XMLHttpRequest();
+        xhReq.open("POST",url.split("?")[0], false);
+        xhReq.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xhReq.send(url.substr(url.indexOf("?")+1));
+        var serverResponse = xhReq.responseText;
+        return(serverResponse); 
+}
+
+function discuss(url,dest){
+        console.log("discuss");
+        var res = ajax("?f=discuss&url="+encodeURI(url)); 
+        if(res == "" || res == null){
+                return false;
+        } 
+        var obj = JSON.parse(res);
+        var i;
+        var d = dest.parentNode;
+        for ( i = 0; i < obj["data"]["children"].length; i++) {
+                if(dest.innerText != obj["data"]["children"][i]["data"]["title"]){
+                        var title = obj["data"]["children"][i]["data"]["title"];
+                }else{
+                        var title = "Discuss this @ /r/"+obj["data"]["children"][i]["data"]["subreddit"];
+                }
+                var span = document.createElement( 'span' );
+                span.innerHTML = "<br><span class='discuss'><a class='discusslink' href='http://reddit.com"+ obj["data"]["children"][i]["data"]["permalink"] +"' target='_blank'>"+title+"</a>&nbsp{"+obj["data"]["children"][i]["data"]["num_comments"]+"}&nbsp["+ obj["data"]["children"][i]["data"]["score"] +"]</span>";
+                d.appendChild(span); 
+        }
+        if(i == 0){
+                return false;
+        }
+        return true;
+}
+
+function updatecounter(){
+        counter = parseInt(document.getElementById("unseencounter").innerHTML);
+        document.getElementById("unseencounter").innerHTML =  --counter;
+        document.title = "RIFTS ("+counter+")";
+}
+
+</script>
 
