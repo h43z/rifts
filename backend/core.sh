93524e424218e8370f6353a04b7107255ff7ab09
h43z <h43zet@gmail.com>
2013-09-26 22:35:21 +0200

    removed all ulrs to lowercase...why? added in first place.

--- backend/core.sh
+++ backend/core.sh
@@ -43,7 +43,8 @@ parse(){
 					location=$(wget -t 1 -T 7 -U notgoogle --no-check-certificate -S --spider $location 2>&1 | grep "Location:" | tr "\n" "|")
 					location=$(echo $location | grep -Po '(?<=Location: ).*?(?=\|)' | awk '{print $1}' | tail -n1 )		
 				fi
-					location=$(echo $location | tr '[A-Z]' '[a-z]')
+					location=$(echo $location)
+					##| tr '[A-Z]' '[a-z]')
 					line="$title###$location"
 					echo "$_PARAMETERURL###$line" >> $_CACHE
 					addnews "$line"
@@ -88,6 +89,7 @@ if grep -q "$_PARAMETERURL" $_CACHE ;then
 	grep "$_PARAMETERURL" $_CACHE | awk -F'###' '{print $2"###"$3}' | 
 		while read line
 		do
+			echo "debug: $line"
 			addnews "$line"
 		done
 else
